9d3da4770febeb61b495502d8ed25de3cf33c99b
0x47726579 <lscherer@protonmail.com>
2020-09-26 15:22:02 +0200

    new work branch

---
+++ site/html/fragments/footer.php
@@ -0,0 +1,13 @@
+<div class="clear"></div>
+
+<div id="footer">Copyright &copy; 2014. Design by <a href="http://www.htmltemplates.net/" target="_blank">HTML
+        Templates</a><br>
+
+</div>
+
+</div>
+
+</div>
+
+</body>
+</html>
---
+++ site/html/fragments/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>STI - Projet 1</title>
+    <meta name="description" content="A simple chatting platform">
+    <meta name="keywords" content="chat, STI, security">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<div id="wrapper">
+
+    <div id="header">
+
+        <div class="top_banner">
+            <h1>STI - Projet 1</h1>
+            <p>Application de communication sécurisée</p>
+        </div>
+
+        <div class="navigation">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="about.php">Contact</a></li>
+                <li><a href="#">New Page 1</a></li>
+                <li><a href="#">New Page 2</a></li>
+                <li><a href="#">New Page 3</a></li>
+                <li><a href="#">New Page 4</a></li>
+            </ul>
+        </div>
+
+    </div>
