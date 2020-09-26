c109e7a4109120fe24e78364a3a35351a415bca2
0x47726579 <lscherer@protonmail.com>
2020-09-26 14:59:43 +0200

    first commit

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+
+docker kill sti_project
+docker rm sti_project
+
+echo "[+] Building and running docker..."
+
+sleep 1
+docker.exe run -ti -v "/${PWD}/site/":/usr/share/nginx/ -d -p 8080:80 --name sti_project --hostname sti arubinst/sti:project2018 
+
+sleep 2
+echo "[+] Starting nginx service"
+docker exec -u root sti_project service nginx start
+echo "[+] Starting php service"
+docker exec -u root sti_project service php5-fpm start
+echo "[*] Done"
+
+sleep 10
