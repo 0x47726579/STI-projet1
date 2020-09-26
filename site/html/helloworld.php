96107b31df4f6c669519e62fdc4f7215ad5e6f99
0x47726579 <lscherer@protonmail.com>
2020-09-26 15:18:48 +0200

    first commit

---
+++ site/html/helloworld.php
@@ -0,0 +1,13 @@
+<!-- header goes here -->
+<?php
+include('fragments/header.php');
+?>
+
+<?php echo '<p>Hello World</p>';
+$version = phpversion();
+print $version; ?>
+
+<!-- footer goes here -->
+<?php
+include('fragments/footer.php');
+?>
