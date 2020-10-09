f2f5ca23acfeb6f213bec79d429199ee10c52df2
CassandreWoj <cassandre.wojciechowski@heig-vd.ch>
2020-10-09 16:18:58 +0200

    update reply, pb rencontrés ...

--- site/html/reply_msg.php
+++ site/html/reply_msg.php
@@ -7,7 +7,7 @@ include('fragments/header.php');
 <?php
 $db = new PDO('sqlite:/usr/share/nginx/databases/database.sqlite');
 
-// finds the message to delete with its id
+// finds the message to reply to with its id
 $sql = 'SELECT * FROM message WHERE messageID = "' . $_GET['messageID'] . '";';
 
 $ret = $db->query($sql);
@@ -29,7 +29,7 @@ $ret = $db->query($sql);
             <!-- print the message we want to reply to -->
             <div class = row>
                 <div class = col>
-                    <table class="table-bordered">
+                    <table class="table">
                         <thead>
                             <tr>
                                 <th>Sender</th>
@@ -39,24 +39,44 @@ $ret = $db->query($sql);
                             </tr>
                         </thead>
                         <tbody>
-                        <?php foreach($ret as $row): ?>
+                        <?php
+                            foreach($ret as $row):
+                            $sender = $row['sender'];
+                            $object = $row['object'];
+                            ?>
                             <tr>
                                 <td> <?php echo $row['sender']?> </td>
                                 <td> <?php echo $row['date']?> </td>
                                 <td> <?php echo $row['object']?> </td>
                                 <td> <?php echo $row['text']?> </td>
                             </tr>
-                        <?php endforeach;?>
+                        <?php endforeach; ?>
                         </tbody>
                     </table>
+
+                    <!-- TODO: create window to enter the reply -->
+                    <form>
+                        <label for="reply">Reply:</label><br>
+                        <textarea cols="35" rows="10" name="reply">Type your reply here ...</textarea>
+                        <br><br>
+                        <input type="submit" value="Send">
+                    </form>
+
+                    <?php
+
+                    $db = new PDO('sqlite:/usr/share/nginx/databases/database.sqlite');
+                    // inserts the reply message in the database
+                    // TODO : je ne sais pas comment utiliser le texte écrit en réponse ...
+                    $sql = 'INSERT INTO message VALUES( "' . $_GET['messageID'] . '" + 1, datetime(), "' . $_SESSION["login"] . '", "' . $sender . '", "RE: " || "' . $object . '", "' . reply . '" ;';
+                    $ret = $db->query($sql);
+
+                    ?>
+
                 </div>
             </div>
-
-            <!-- to do : create window to enter the reply -->
-
-
         </div>
     </div>
+</div>
 
     <!-- footer goes here -->
     <?php
