bc556f04d35fc0d31b5cda71852a747846cff550
0x47726579 <lscherer@protonmail.com>
2020-10-15 11:41:21 +0200

    refactored print_msg.php

--- site/html/mailbox.php
+++ site/html/mailbox.php
@@ -15,41 +15,46 @@
     $ret = $sth->fetchAll();
     //    var_dump($ret);
 ?>
-
-<div class="right_section">
-    <div class="common_content">
-        <h1>
-            Your mailbox
-        </h1>
-        <div class=row>
-            <div class=col>
-                <table class="table-bordered">
-                    <thead>
-                    <tr>
-                        <th>Sender</th>
-                        <th>Date</th>
-                        <th>Subject</th>
-                        <th colspan="3">Actions</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php foreach ($ret as $row): ?>
+<?php if (isset($_GET) && $_GET['read'])
+{
+    include_once('print_msg.php');
+} else
+{ ?>
+    <div class="right_section">
+        <div class="common_content">
+            <h1>
+                Your mailbox
+            </h1>
+            <div class=row>
+                <div class=col>
+                    <table class="table-bordered">
+                        <thead>
                         <tr>
-                            <td> <?= $row['username'] ?> </td>
-                            <td> <?= $row['messageDate'] ?> </td>
-                            <td> <?= $row['object'] ?> </td>
-                            <td><a href="print_msg.php?messageID=<?= $row['messageID'] ?>"> Read </a></td>
-                            <td><a href="reply_msg.php?messageID=<?= $row['messageID'] ?>"> Reply </a></td>
-                            <td><a href="delete_msg.php?messageID=<?= $row['messageID'] ?>"> Delete </a></td>
+                            <th>Sender</th>
+                            <th>Date</th>
+                            <th>Subject</th>
+                            <th colspan="3">Actions</th>
                         </tr>
-                    <?php endforeach; ?>
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                        <?php foreach ($ret as $row): ?>
+                            <tr>
+                                <td> <?= $row['username'] ?> </td>
+                                <td> <?= $row['messageDate'] ?> </td>
+                                <td> <?= $row['object'] ?> </td>
+                                <td><a href="mailbox.php?read=true&amp;messageID=<?= $row['messageID'] ?>"> Read </a>
+                                </td>
+                                <td><a href="reply_msg.php?messageID=<?= $row['messageID'] ?>"> Reply </a></td>
+                                <td><a href="delete_msg.php?messageID=<?= $row['messageID'] ?>"> Delete </a></td>
+                            </tr>
+                        <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
-</div>
-
+<?php } ?>
 <!-- footer goes here -->
 <?php
     include('fragments/footer.php');
--- site/html/print_msg.php
+++ site/html/print_msg.php
@@ -1,25 +1,13 @@
-<!-- header goes here -->
 <?php
-    include('fragments/header.php');
-?>
-
-<?php
-    $db = new PDO('sqlite:/usr/share/nginx/databases/database.sqlite');
 
     // finds the message to print from the table
-    $sql = 'SELECT u.username, m.messageDate, m.object, m.message, m.messageID 
+    $sth = $db->prepare('SELECT u.username, m.messageDate, m.object, m.message, m.messageID 
         FROM message AS m
-        INNER JOIN users as u on u.id = m.senderID
-        WHERE messageID = "' . $_GET['messageID'] . '";';
+        INNER JOIN users as u ON u.id = m.senderID
+        WHERE messageID = ?;');
+    $sth->execute(array($_GET['messageID']));
+    $ret = $sth->fetchAll();
 
-    $ret = $db->query($sql);
-
-?>
-
-
-<!-- left side bar goes here -->
-<?php
-    include('fragments/left_side_bar.php');
 ?>
 
 <div class="right_section">
@@ -30,31 +18,30 @@
         <div class=row>
             <div class=col>
                 <table class="table-bordered">
-                    <thead>
-                    <tr>
-                        <th>Sender</th>
-                        <th>Date</th>
-                        <th>Subject</th>
-                        <th>Message</th>
-                    </tr>
-                    </thead>
-                    <tbody>
                     <?php foreach ($ret as $row): ?>
+
+                        <thead>
                         <tr>
-                            <td> <?php echo $row['username'] ?> </td>
-                            <td> <?php echo $row['messageDate'] ?> </td>
-                            <td> <?php echo $row['object'] ?> </td>
-                            <td> <?php echo $row['message'] ?> </td>
+                            <th>Subject :</th>
+                            <td colspan="3"> <?php echo $row['object'] ?> </td>
                         </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th>From :</th>
+                            <td> <?php echo $row['username'] ?> </td>
+                            <th>Sent :</th>
+                            <td> <?php echo $row['messageDate'] ?> </td>
+                        </tr>
+                        <tr>
+                            <td colspan="4"> <?php echo $row['message'] ?> </td>
+                        </tr>
+                        </tbody>
                     <?php endforeach; ?>
-                    </tbody>
                 </table>
+
+
             </div>
         </div>
     </div>
 </div>
-
-<!-- footer goes here -->
-<?php
-    include('fragments/footer.php');
-?>
