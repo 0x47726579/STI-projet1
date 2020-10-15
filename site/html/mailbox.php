70ae7e9d134a459882d6c6eb36e4e3bc6c2d8ad2
0x47726579 <lscherer@protonmail.com>
2020-10-15 15:00:17 +0200

    I think I'm done? I'm free? I'm... free? I'M FREE!

--- site/html/mailbox.php
+++ site/html/mailbox.php
@@ -25,78 +25,95 @@
         <hr>
         <div class=row>
             <div class=col>
-                <?php if (isset($_GET) && $_GET['messageID']
-                    && ($_GET['read'] || $_GET['reply']))
-                {
-                    include_once('functions/message.php');
-                    $message = new message($_GET['messageID']);
-
-
-                    if ($_GET['reply'])
+                <?php
+                    // if we're not simply looking at the mailbox we want to do an action :
+                    // either read a mail, or reply to one
+                    // this is done through different GET variables set to true.
+                    if (isset($_GET) && $_GET['messageID']
+                        && ($_GET['read'] || $_GET['reply']))
                     {
-                        if (isset($_POST) && $_POST['send'] == "Send")
+                        include_once('functions/message.php');
+                        $message = new message($_GET['messageID']);
+
+
+                        if ($_GET['reply'])
                         {
-                            $result = $message->send_message($message->getSenderID(),
-                                $message->getRecipientID(),
-                                "RE: " . $message->getObject(),
-                                $_POST['reply']);
-                            if ($result)
+                            // we got a POST request to send the reply, execute it then tell the user about the result
+                            if (isset($_POST) && $_POST['send'] == "Send")
                             {
-                                echo "Reply Sent !";
-                            } else
-                            {
-                                print_r("Oops ... Something went wrong! The message has not been sent.");
+                                $result = $message->send_message($message->getSenderID(),
+                                    $message->getRecipientID(),
+                                    "RE: " . $message->getObject(),
+                                    $_POST['reply']);
+                                if ($result)
+                                {
+                                    echo "Reply Sent !";
+                                } else
+                                {
+                                    print_r("Oops ... Something went wrong! The message has not been sent.");
+                                }
                             }
+
+                            ?>
+                            <!-- simple form to reply to a mail -->
+                            <form action="mailbox.php?reply=true&amp;messageID=<?= $message->getMessageID() ?>"
+                                  method="POST" id="form">
+                                <input type="hidden" name="oldMsgID" value="<?php echo $message->getMessageID() ?>"/>
+                                <label for="reply"> Write a reply :</label>
+
+                                <textarea cols="100%"
+                                          rows="8"
+                                          id="reply"
+                                          name="reply"
+                                          placeholder="Type your reply here ..."
+                                          required></textarea>
+
+                                <input type="submit" name="send" value="Send">
+                            </form>
+                            <?php
+
                         }
+                        // we show the message we're either replying to or wanting to read
+                        $message->print_message();
+                        if ($_GET['read'])
+                        {
+                            ?>
+                            <a href="mailbox.php?reply=true&amp;messageID=<?= $message->getMessageID() ?>" class="btn">
+                                Reply </a>
+                            <a href="delete_msg.php?messageID=<?= $message->getMessageID() ?>" class="btn">
+                                Delete </a>
 
-                        ?>
-                        <form action="mailbox.php?reply=true&amp;messageID=<?= $message->getMessageID() ?>"
-                              method="POST" id="form">
-                            <input type="hidden" name="oldMsgID" value="<?php echo $message->getMessageID() ?>"/>
-                            <label for="reply"> Write a reply :</label>
-
-                            <textarea cols="100%"
-                                      rows="8"
-                                      id="reply"
-                                      name="reply"
-                                      placeholder="Type your reply here ..."
-                                      required></textarea>
-
-                            <input type="submit" name="send" value="Send">
-                        </form>
-                        <?php
-
-                    }
-                    $message->print_message();
-                } else
-                { ?>
-                    <table class="table-bordered">
-                        <thead>
-                        <tr>
-                            <th>Sender</th>
-                            <th>Date</th>
-                            <th>Subject</th>
-                            <th colspan="3">Actions</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <?php foreach ($ret as $row): ?>
+                            <?php
+                        }
+                    } else
+                    { ?>
+                        <table class="table-bordered">
+                            <thead>
                             <tr>
-                                <td> <?= $row['username'] ?> </td>
-                                <td> <?= $row['messageDate'] ?> </td>
-                                <td> <?= $row['object'] ?> </td>
-                                <td><a href="mailbox.php?read=true&amp;messageID=<?= $row['messageID'] ?>">
-                                        Read </a>
-                                </td>
-                                <td><a href="mailbox.php?reply=true&amp;messageID=<?= $row['messageID'] ?>">
-                                        Reply </a>
-                                </td>
-                                <td><a href="delete_msg.php?messageID=<?= $row['messageID'] ?>"> Delete </a></td>
+                                <th>Sender</th>
+                                <th>Date</th>
+                                <th>Subject</th>
+                                <th colspan="3">Actions</th>
                             </tr>
-                        <?php endforeach; ?>
-                        </tbody>
-                    </table>
-                <?php } ?>
+                            </thead>
+                            <tbody>
+                            <?php foreach ($ret as $row): ?>
+                                <tr>
+                                    <td> <?= $row['username'] ?> </td>
+                                    <td> <?= $row['messageDate'] ?> </td>
+                                    <td> <?= $row['object'] ?> </td>
+                                    <td><a href="mailbox.php?read=true&amp;messageID=<?= $row['messageID'] ?>">
+                                            Read </a>
+                                    </td>
+                                    <td><a href="mailbox.php?reply=true&amp;messageID=<?= $row['messageID'] ?>">
+                                            Reply </a>
+                                    </td>
+                                    <td><a href="delete_msg.php?messageID=<?= $row['messageID'] ?>"> Delete </a></td>
+                                </tr>
+                            <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    <?php } ?>
             </div>
         </div>
     </div>
